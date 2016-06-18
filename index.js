fe52ca56287ddb8d7a347a156b3b958f3d6c60b1
bredele <olivier.wietrich@gmail.com>
2016-06-18 12:46:15 -0600

    at some point we will have a cli to fetch projects

--- index.js
+++ index.js
@@ -5,9 +5,20 @@
 var vomit = require('vomit');
 
 /**
+ * Return DOM list of my projects.
  *
+ * @param {Object} data
+ * @api public
  */
 
 module.exports = function(data) {
   data = data || require('./projects.json');
+  return vomit('ul', data.projects.forEach(function(project) {
+    return vomit('li', [
+      vomit('h3', project.name),
+      vomit('p', {
+        class: 'summary'
+      }, project.summary)
+    ]);
+  }));
 };
